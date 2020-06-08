098379ac2a060066781f77f45d19d0306df59532
aperture13 <61824266+aperture13@users.noreply.github.com>
2020-06-08 16:52:23 +0300

    learn

---
+++ Learning/Learning_Project_1/Learning_Project_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+//using namespace std;
+
+int main()
+{
+	int fav_numb;
+	std::cout << "Enter a number between 1 and 100"<< std::endl;
+	std::cin >> fav_numb;
+	std::cout << "That is my favorite number too!" << std::endl;
+	
+	system("pause");
+	return 0;
+}
