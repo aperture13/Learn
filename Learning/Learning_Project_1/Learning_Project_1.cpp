b536207024df88d09e6b5cb3b8b9a164a318e9b6
aperture13 <61824266+aperture13@users.noreply.github.com>
2020-06-09 17:52:53 +0300

    Update Learning_Project_1.cpp
    Learning Basic IO

--- Learning/Learning_Project_1/Learning_Project_1.cpp
+++ Learning/Learning_Project_1/Learning_Project_1.cpp
@@ -1,18 +1,49 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-	int fav_num;
-	do
-	{
-		std::cout << "Enter your favorite number between 1 and 100:";
-		std::cin >> fav_num;
-	}
-	while (fav_num > 100 || fav_num < 1);
+	// cout << "Hello World!"<<endl;
+
+	// cout << "Hello";
+	// cout << "World" << endl;
+
+	// cout << "Hello World" << endl;
+	// cout << "Hello" << "World"<<endl;
+	// cout << "Hello" << "World\n";
+	// cout << "Hello\nOut\nThere\n";
+
+	int num1;
+	int num2;
+	double num3;
+
+	// cout << "Enter an integer:";
+	// cin >> num1;
+	// cout << "You entered " << num1 << endl;
+
+	// cout << "Enter first integer: ";
+	// cin >> num1;
+	//
+	// cout << "Enter a second integer: ";
+	// cin >> num2;
+	//
+	// cout << "You entered " << num1 << " and " << num2 << endl;
+
+	// cout<<"Enter 2 integers separated by a space ";
+	// cin>>num1>>num2;
+	// cout<<"You entered "<<num1<<" and "<<num2<<endl;
 	
-	std::cout << "Your favorite number is "<<fav_num << "?"<<std::endl<<"That's my favorite number too!" << std::endl;
-	
-	system("pause");
+	// cout << "Enter a double ";
+	// cin >> num3;
+	// cout << "You entered: " << num3 << endl;
+
+	cout << "Enter an integer ";
+	cin >> num1;
+	cout << "Enter a double ";
+	cin >> num3;
+
+	cout << "The integer is " << num1 << endl << "The double is " << num3 << endl;
 	
 	return 0;
 }
