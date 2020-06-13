830e1db81002119676014de9cf3132e66aa01be8
aperture13 <61824266+aperture13@users.noreply.github.com>
2020-06-13 16:45:19 +0300

    Update Pancakes Example.cpp

--- Pancakes Example/Pancakes Example.cpp
+++ Pancakes Example/Pancakes Example.cpp
@@ -6,35 +6,46 @@ using std::endl;
 
 int main()
 {
-	int_fast64_t number_of_cupcakes[10];
-	int_fast64_t order_of_cupcakes[10];
-	int counter1=0, counter2=0;
+	int number_of_pancakes[10];
+	int order_of_pancakes[10];
+	int count1, count2;
+	int temp;
 
-	for (counter1 = 0; counter1 < 10; counter1++);
+	for (count1=0;count1<10;count1++)
 	{
-		order_of_cupcakes[counter1] = 0;
-		number_of_cupcakes[counter1] = 0;
+		number_of_pancakes[count1] = 0;
+		order_of_pancakes[count1] = 0;
 	}
-	for (counter1=0; counter1<10; counter1++)
+
+	for (count1=0;count1<10;count1++)
 	{
-		cout << "Enter the number of pancakes eaten by person " << counter1 + 1<<" ";
-		cin >> number_of_cupcakes[counter1];
-		for (counter2=0; counter2<10; counter2++)
+		cout << "Enter the number of pancakes that person " << count1 + 1 << " ate: ";
+		cin >> number_of_pancakes[count1];
+	}
+
+	for (count1=0;count1<10;count1++)
+	{
+		temp = 0;
+		for (count2=count1+1;count2<10;count2++)
 		{
-			if (number_of_cupcakes[counter2] > number_of_cupcakes[9 - counter2])
+			if (number_of_pancakes[count1]<number_of_pancakes[count2])
 			{
-				if (number_of_cupcakes[counter2] > number_of_cupcakes[counter2 - 1])
-					order_of_cupcakes[counter2] = counter2;
+				temp = count1;
+			}
+			else
+			{
+				temp = count1;
 			}
 		}
+		order_of_pancakes[count1] = temp;
 	}
-	for (counter1=0;counter1<10;counter1++)
-	{
-		cout << "Person " << order_of_cupcakes[counter1+1] << " ate "
-		<< number_of_cupcakes[order_of_cupcakes[counter1]] <<" cupcakes!" << endl;
-
-	}
-
+	
+	for (count1 = 0; count1 < 10; count1++)
+		cout << number_of_pancakes[count1]<<" ";
+	cout << endl;
+	for (count1 = 0; count1 < 10; count1++)
+		cout << order_of_pancakes[count1]<<" ";
+	cout << endl;
 	
 	system("pause");
 	return 0;
