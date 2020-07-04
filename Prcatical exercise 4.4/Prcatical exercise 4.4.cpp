783ea56782c5d5550e616b49528f943aa1d03dbe
aperture13 <61824266+aperture13@users.noreply.github.com>
2020-07-04 19:51:23 +0300

    Learning from Practical c++ book

---
+++ Prcatical exercise 4.4/Prcatical exercise 4.4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "*   *\t*****\t*    \t*    \t*****" <<endl;
+	cout << "*   *\t*    \t*    \t*    \t*   *" << endl;
+	cout << "*   *\t*    \t*    \t*    \t*   *" << endl;
+	cout << "*****\t*****\t*    \t*    \t*   *" << endl;
+	cout << "*   *\t*    \t*    \t*    \t*   *" << endl;
+	cout << "*   *\t*    \t*    \t*    \t*   *" << endl;
+	cout << "*   *\t*****\t*****\t*****\t*****" << endl;
+
+	system("pause");
+	return 0;
+}
